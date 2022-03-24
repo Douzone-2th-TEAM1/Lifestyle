375b47f65b4def3f0c573c320bb2b28e7afb88d0
chaeyeon <pcyeon07@gmail.com>
2022-03-24 12:19:21 +0900

    Fix: 메인 이동시 스크롤 상단 고정, 리빙 페이지 수정

--- js/main.js
+++ js/main.js
@@ -15,12 +15,27 @@ var loadMain = () => {
   if (sessionStorage.getItem('loginFlag') === 'true') {
     $(document).ready(function () {
       $('#root-body').load('/html/main.html');
+      $('html, body').stop().animate({ scrollTop: 0 }, 0);
     });
   } else {
     alert('잘못된 접근입니다.');
     sessionStorage.setItem('loginFlag', false);
     $(document).ready(function () {
       $('#root-body').load('/html/JSB/SB-login.html');
+      $('html, body').stop().animate({ scrollTop: 0 }, 0);
     });
   }
+};
+
+var livigPage = () => {
+  $(document).ready(function () {
+    $('#main-contents').load('../../html/pcy/listpage.html');
+  });
+};
+
+var livingArticlePage = () => {
+  $(document).ready(function () {
+    $('#main-contents').load('/html/pcy/articlePage.html');
+    $('html, body').stop().animate({ scrollTop: 0 }, 0);
+  });
 };
